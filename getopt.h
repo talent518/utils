68b3bef8cc2727bdb0689eecf193a79ea3fe248e
阿宝 <talent518@yeah.net>
2017-03-22 22:14:14 +0800

    获取指定格式的命令行参数(参数识别/粘贴)

---
+++ getopt.h
@@ -0,0 +1,24 @@
+#ifndef GETOPT_H
+#define GETOPT_H
+
+#ifdef NETWARE
+/*
+As NetWare LibC has optind and optarg macros defined in unistd.h our local variables were getting mistakenly preprocessed so undeffing optind and optarg
+*/
+#undef optarg
+#undef optind
+#endif
+
+/* Define structure for one recognized option (both single char and long name).
+ * If short_open is '-' this is the last option. */
+typedef struct _opt_struct {
+	char opt_char;
+	int  need_param;
+	char * opt_name;
+} opt_struct;
+
+/* holds the index of the latest fetched element from the opts array */
+extern int optidx;
+int getopt(int argc, char* const *argv, const opt_struct opts[], char **optarg, int *optind, int show_err, int arg_start);
+
+#endif
