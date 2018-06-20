89489a4304b3d9e28a49c57f489b434f8a4a6011
阿宝 <talent518@yeah.net>
2018-06-20 22:37:31 +0800

    解决crypt.h中的inline函数连接时报错crypt_decode未定义问题。

--- crypt.h
+++ crypt.h
@@ -6,7 +6,7 @@
 
 unsigned int crypt_code(const char *str, unsigned int len, char **ret, const char *key, bool mode, unsigned int expiry);
 
-inline char *crypt_encode(const char *str, unsigned int len, const char *key, unsigned int expiry) {
+static inline char *crypt_encode(const char *str, unsigned int len, const char *key, unsigned int expiry) {
 	char *enc = NULL;
 
 	crypt_code(str, len, &enc, key, false, expiry);
@@ -14,7 +14,7 @@ inline char *crypt_encode(const char *str, unsigned int len, const char *key, un
 	return enc;
 }
 
-inline unsigned int crypt_decode(const char *dec, char **data, const char *key, unsigned int expiry) {
+static inline unsigned int crypt_decode(const char *dec, char **data, const char *key, unsigned int expiry) {
 	return crypt_code(dec, strlen(dec), data, key, true, expiry);
 }
 
