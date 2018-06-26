28fb91340c56bb1f4c298c48c2cc7e78fc4d484d
ZhangBaoCai <talent518@yeah.net>
2018-06-26 14:11:05 +0800

    优化不重复序列代码。

--- nonRepetitiveSequence.c
+++ nonRepetitiveSequence.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 void _seq(char *str, int b, int n) {
-	static int id = 0;
+	static int id;
+	static char fmt[12];
 	register char chr;
 	register int i;
+	if(b == 0) {
+		id = 1;
+		for(i=n;i>1;i--) {
+			id *= i;
+		}
+		sprintf(fmt, "%%%dd: %%s\n", (int) ceil(log10(id+1)));
+		id = 0;
+	}
 	
 	for(i=b; i<n; i++) {
 		if(i>b) {
@@ -15,9 +25,9 @@ void _seq(char *str, int b, int n) {
 		}
 		
 		if(b == n-2) {
-			printf("%10d: %s\n", ++id, str);
+			printf(fmt, ++id, str);
 		} else {
-			printf("b: %d\n", b);
+			// printf("b: %d\n", b);
 			memcpy(str+n+1, str, n);
 			_seq(str+n+1, b+1, n);
 		}
