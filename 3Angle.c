8426ca09f1e9def9a4f2b4b2dc8689ed8dc81adb
ZhangBaoCai <talent518@yeah.net>
2018-11-19 23:50:48 +0800

    优化。

--- 3Angle.c
+++ 3Angle.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define N 10
-
 int main(int argc, char *argv[]) {
+	int N = 10;
 	int i, j;
+	
+	if(argc == 2 && (N = atoi(argv[1]))<3) {
+		N = 3;
+	}
 	
 	for(i=0; i<N; i++) {
 		for(j=1; j<N-i; j++) {
