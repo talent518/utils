e1dc85748186b049c7ead8941f49f7501ca9670b
阿宝 <talent518@yeah.net>
2019-01-04 01:34:08 +0800

    大数十进制转十六进制

---
+++ algo-dec2hex.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char hex[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+int hex2dec(char h) {
+	if(h>='0' && h<='9') return h - '0';
+	if(h>='A' && h<='F') return h - 'A';
+	if(h>='a' && h<='f') return h - 'a';
+	return 0;
+}
+int main(int argc, char *argv[]) {
+	register char *p, *pp, *p0;
+	register int n;
+	char *ap, *bp, *bin;
+	
+	if(argc != 2) {
+		goto usage;
+	}
+	
+	p = argv[1];
+	if(*p < '1' || *p > '9' && *p < 'A' || *p > 'F' && *p < 'a' || *p > 'f') {
+		goto usage;
+	}
+	while(*p >= '0' && *p <= '9' || *p >= 'A' && *p <= 'F' || *p >= 'a' && *p <= 'f') p++;
+	if(*p) goto usage;
+	
+	ap = strdup(argv[1]);
+	n = strlen(ap);
+	bp = (char *) malloc(n+1);
+	p0 = bp;
+	*p0 = '\0';
+	
+	n = n + 2 + n/10;
+	bin = (char *) malloc(n+1);
+	pp = bin + n;
+	*pp = '\0';
+	memset(bin, '0', n);
+	pp--;
+	
+loop:
+	p = ap;
+	n = 0;
+	for(; *p; p++) {
+		if(n) {
+			n = n * 10 + hex2dec(*p);
+		} else {
+			n = hex2dec(*p);
+		}
+		*p0++ = hex[n / 16];
+		n = n % 16;
+	}
+	*p0 = '\0';
+	*pp-- = hex[n];
+	p = bp;
+	while(*p == '0') p++;
+	if(*p) {
+		strcpy(ap, p);
+		p0 = bp;
+		*p0 = '\0';
+		n = 0;
+		goto loop;
+	}
+	
+	p = bin;
+	while(*p == '0') p++;
+	
+	printf("%s hexadecimal value is %s\n", argv[1], p);
+	
+	free(ap);
+	free(bp);
+	free(bin);
+	
+	return 0;
+usage:
+	printf("usage: %s <number>\n", argv[0]);
+	return 1;
+}
+
