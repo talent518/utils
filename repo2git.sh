7feac2fc54da8710588aa943ef4fdc3afe421c9e
阿宝 <talent518@yeah.net>
2021-05-15 01:23:29 +0800

    Fix bug: repo2git.sh

--- repo2git.sh
+++ repo2git.sh
@@ -3,24 +3,30 @@
 set -e
 
 I=0
-repo forall -c 'echo "$REPO_PATH" "$(echo $REPO_RREV|cut -d / -f3)"' | while read dir ver; do
+$(repo forall -c 'echo "$REPO_PATH" "$(echo $REPO_RREV|cut -d / -f3)"' | while read dir ver; do
 	I=$(expr $I + 1)
-	echo -e "\033[31m$I\e[0m \033[33m$dir\033[0m"
+	echo
+	echo -e "\033[31m$I\e[0m \033[33m$dir\033[0m" >&2
 	if [ -f "$dir.lock" ]; then
 		continue
 	fi
 
 	N=0
-	sh -c "cd '$dir';git ls-files" | while read line; do
+	N=$(sh -c "cd '$dir';git ls-files" | while read line; do
 		N=$(expr $N + 1)
-		echo -n -e "\033[2K$N files\r"
-		git add -f "$dir/$line"
-	done
+		echo -n -e "\033[2K$N\r" >&2
+		git add -f "$dir/$line" >&2
+		echo
+	done | wc -l)
 	
-	echo -n -e "\033[2K"
+	echo -n -e "\033[2K" >&2
 
-	git repack --max-pack-size 500M
-	git commit -a -m"$ver: $dir"
-	touch "$dir.lock"
-done
+	if [ $N -gt 0 ]; then
+		git repack --max-pack-size 500M >&2
+		git commit -a -m"$ver: $dir" >&2
+		touch "$dir.lock" >&2
+	fi
+done | wc -l)
+
+echo "completed $I projects"
 
