bce61690a76163fd1dbe338d56d43c05ec7cef88
阿宝 <talent518@yeah.net>
2021-05-15 19:00:43 +0800

    优化性能。

--- repo2git.sh
+++ repo2git.sh
@@ -7,24 +7,30 @@ $(repo forall -c 'echo "$REPO_PATH" "$(echo $REPO_RREV|cut -d / -f3)"' | while r
 	I=$(expr $I + 1)
 	echo
 	echo -e "\033[31m$I\e[0m \033[33m$dir\033[0m" >&2
+
 	if [ -f "$dir.lock" ]; then
+		echo $(sh -c "cd '$dir';git ls-files"|wc -l) files >&2
 		continue
 	fi
 
-	N=0
-	N=$(sh -c "cd '$dir';git ls-files" | while read line; do
-		N=$(expr $N + 1)
-		echo -n -e "\033[2K$N\r" >&2
-		git add -f "$dir/$line" >&2
-		echo
-	done | wc -l)
-	
-	echo -n -e "\033[2K" >&2
+	sh -c "cd '$dir';git ls-files" > repo2git.lst
+	C=$(cat repo2git.lst|wc -l)
 
-	if [ $N -gt 0 ]; then
+	if [ $C -gt 0 ]; then
+		N=0
+		cat repo2git.lst | while read line; do
+			N=$(expr $N + 1)
+			echo -n -e "\033[2K$(expr $N \* 100 / $C)% => $N/$C\r" >&2
+			echo "'$dir/$line'"
+		done | xargs -n100 git add -f
+
+		echo -e "\033[2K$C files">&2
+
 		git repack --max-pack-size 500M >&2
 		git commit -a -m"$ver: $dir" >&2
 		touch "$dir.lock" >&2
+	else
+		echo -e "\033[34m0 files\033[0m" >&2
 	fi
 done | wc -l)
 
