dce121c10fc38590debe8856ca7399bf69f8ce03
阿宝 <talent518@yeah.net>
2021-07-09 10:08:30 +0800

    Optimize screen2bmp.c

--- screen2bmp.c
+++ screen2bmp.c
@@ -11,12 +11,29 @@
 #include <asm/types.h> 
 #include <linux/videodev2.h>
 #include <linux/fb.h>
- 
- 
+
+#include <math.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+
+double microtime() {
+	struct timeval tp = {0};
+
+	if (gettimeofday(&tp, NULL)) {
+		return 0;
+	}
+
+	return (double)(tp.tv_sec + tp.tv_usec / 1000000.00);
+}
+
+volatile unsigned int is_running = 1;
 static unsigned int capframe = 0;
+static unsigned int maxframe = 0;
+static unsigned char format[30];
 static unsigned char filename[30];
  
-unsigned char bmp_head_t[] = {
+unsigned char bmp_head_66[] = {
 	0x42, 0x4d, 0x42, 0x58, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00,
 	0x42, 0x00, 0x00, 0x00, 0x28, 0x00, 0x00, 0x00, 0xf0, 0x00,
 	0x00, 0x00, 0x40, 0x01, 0x00, 0x00, 0x01, 0x00, 0x10, 0x00,
@@ -26,7 +43,7 @@ unsigned char bmp_head_t[] = {
 	0x00, 0x00, 0x1f, 0x00, 0x00, 0x00
 };
 
-unsigned char bmp_head[54];
+unsigned char bmp_head_54[54];
 static char *fb_addr = NULL;
 int width = 0, height = 0;
 int xoffset = 0, xsize = 0;
@@ -63,24 +80,26 @@ static int fb_init(void) {
 		exit(1);
 	}
 
-	printf("%dx%d bpp:%d mmaped %p\n", width, height, fb_bpp, fb_addr);
+	printf("[%lf] size: %dx%d, bpp: %d, mmap: %p\n", microtime(), width, height, fb_bpp, fb_addr);
  
 	return fd;
 }
 
-void save_to_file(unsigned int size) {
+void save_to_file() {
 	FILE *fp;
 
+	printf("[%lf] ", microtime());
+
 	capframe++;
-	sprintf(filename, "screen-%02d.bmp", capframe);
+	sprintf(filename, format, capframe);
 
 	printf("Saving to %s ...\n", filename);
 
 	fp = fopen(filename, "w+");
 	if(fb_bpp == 16)
-		fwrite(bmp_head_t, 1, 66, fp);
+		fwrite(bmp_head_66, 1, 66, fp);
 	else
-		fwrite(bmp_head, 1, 54, fp);
+		fwrite(bmp_head_54, 1, 54, fp);
 
 	for(int y = 0; y < height; y++)
 		fwrite(fb_addr + (height - 1 - y) * xoffset, 1, xsize, fp);
@@ -88,32 +107,63 @@ void save_to_file(unsigned int size) {
 	fclose(fp);
 }
 
-int main(int argc, char *argv[]) {
-	unsigned int i = 10;
-	unsigned int size = 0;
-	int fd = fb_init();
+void signal_handler(int sig) {
+	switch(sig) {
+		case SIGINT:
+			is_running = 0;
+			break;
+		case SIGALRM:
+			if(maxframe--) save_to_file();
+			else is_running = 0;
+			break;
+		default:
+			printf("SIG: %d\n", sig);
+	}
+}
 
-	size = width * height * fb_bpp / 8;
-	printf("size: %u\n", size);
+int main(int argc, char *argv[]) {
+	int fd = fb_init();
  
 	if(fb_bpp == 16){
-	    *((unsigned int*)(bmp_head_t + 18)) = width;
-	    *((unsigned int*)(bmp_head_t + 22)) = height;
-	    *((unsigned short*)(bmp_head + 28)) = 16;
+	    *((unsigned int*)(bmp_head_66 + 18)) = width;
+	    *((unsigned int*)(bmp_head_66 + 22)) = height;
+	    *((unsigned short*)(bmp_head_66 + 28)) = 16;
 	}else{
-		bmp_head[0] = 'B';
-		bmp_head[1] = 'M';
-		*((unsigned int*)(bmp_head + 2)) = (width * fb_bpp / 8) * height + 54;
-		*((unsigned int*)(bmp_head + 10)) = 54;
-		*((unsigned int*)(bmp_head + 14)) = 40;
-		*((unsigned int*)(bmp_head + 18)) = width;
-		*((unsigned int*)(bmp_head + 22)) = height;
-		*((unsigned short*)(bmp_head + 26)) = 1;
-		*((unsigned short*)(bmp_head + 28)) = fb_bpp;
-		*((unsigned short*)(bmp_head + 34)) = (width * fb_bpp / 8) * height;
+		bmp_head_54[0] = 'B';
+		bmp_head_54[1] = 'M';
+		*((unsigned int*)(bmp_head_54 + 2)) = (width * fb_bpp / 8) * height + 54;
+		*((unsigned int*)(bmp_head_54 + 10)) = 54;
+		*((unsigned int*)(bmp_head_54 + 14)) = 40;
+		*((unsigned int*)(bmp_head_54 + 18)) = width;
+		*((unsigned int*)(bmp_head_54 + 22)) = height;
+		*((unsigned short*)(bmp_head_54 + 26)) = 1;
+		*((unsigned short*)(bmp_head_54 + 28)) = fb_bpp;
+		*((unsigned short*)(bmp_head_54 + 34)) = (width * fb_bpp / 8) * height;
 	}
 
-	while(i--) save_to_file(size);
+	if(argc < 2 || (maxframe = atoi(argv[1])) < 1) {
+		maxframe = 10;
+	}
+
+	sprintf(format, "screen-%%0%dd.bmp", (int) floor(log10(maxframe)) + 1);
+
+	struct itimerval itv;
+
+	// Interval for periodic timer
+	// 周期计时器间隔，每次SIGALRM信号事件的时间间隔
+	itv.it_interval.tv_sec = 0;
+	itv.it_interval.tv_usec = 40000; // 40ms
+
+	// Time until next expiration
+	// 下次到期前的时间，首次SIGALRM信号事件的时间间隔
+	itv.it_value.tv_sec = 0;
+	itv.it_value.tv_usec = 40000; // 40ms
+
+	signal(SIGALRM, signal_handler);
+	setitimer(ITIMER_REAL, &itv, NULL);
+	printf("[%lf] start\n", microtime());
+	while(is_running) usleep(1000);
+	printf("[%lf] end\n", microtime());
 
 	munmap(fb_addr, fb_size);
 	close(fd);
