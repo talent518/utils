fc7039b6cb69a2bb6fec836e2fe3fae652aeeb66
阿宝 <talent518@yeah.net>
2021-08-19 10:08:20 +0800

    Fix conn-stat.sh and conn-stat2.sh

--- conn-stat2.sh
+++ conn-stat2.sh
@@ -4,7 +4,7 @@ listen_port_connects() {
         PORT=$1
         NAME=$2
 
-        ss state connected -n 'sport = :'${PORT} | awk 'NR>1{a[$1]++}END{printf "% 9s % 8s % 8s % 5s % 10s % 10s % 10s % 8s % 9s\n", "'${NAME}'", a["SYN-SENT"]+0, a["SYN-RECV"]+0, a["ESTAB"]+0, a["FIN-WAIT-1"]+0, a["CLOSE-WAIT"]+0, a["FIN-WAIT-2"]+0, a["LAST-ACK"]+0, a["TIME-WAIT"]+0;}'
+        ss state connected -n 'sport = :'${PORT} | awk 'NR>1{if($1=="tcp") a[$2]++; else a[$1]++;}END{printf "% 9s % 8s % 8s % 5s % 10s % 10s % 10s % 8s % 9s\n", "'${NAME}'", a["SYN-SENT"]+0, a["SYN-RECV"]+0, a["ESTAB"]+0, a["FIN-WAIT-1"]+0, a["CLOSE-WAIT"]+0, a["FIN-WAIT-2"]+0, a["LAST-ACK"]+0, a["TIME-WAIT"]+0;}'
 }
 
 echo "  program SYN-SENT SYN-RECV ESTAB FIN-WAIT-1 CLOSE-WAIT FIN-WAIT-2 LAST-ACK TIME-WAIT"
