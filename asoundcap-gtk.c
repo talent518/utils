435da44a97a2efbe7de504da2852bde69f92d724
abao <talent518@yeah.net>
2023-01-03 11:46:12 +0800

    Update asoundcap-gtk.c

--- asoundcap-gtk.c
+++ asoundcap-gtk.c
@@ -14,7 +14,6 @@
 #include <alsa/asoundlib.h>
 #include <signal.h>
 #include <pthread.h>
-#include <semaphore.h>
 
 #include <gtk/gtk.h>
 
@@ -23,8 +22,9 @@ snd_pcm_hw_params_t *gp_params;  //设置流的硬件参数
 snd_pcm_uframes_t g_frames; //snd_pcm_uframes_t其实是unsigned long类型
 char *gp_buffer;
 unsigned int g_bufsize;
+int sample_rate = 0, channels = 0, format_size = 0;
 
-int set_hardware_params(int sample_rate, int channels, int format_size) {
+int set_hardware_params() {
 	int rc;
 	
 	/* Open PCM device for playback */
@@ -138,7 +138,6 @@ void sig_handle(int sig) {
 	fprintf(stderr, "\r");
 }
 
-static sem_t sem;
 #define SIZE 128
 static char *bufs[SIZE];
 
@@ -147,42 +146,8 @@ typedef struct {
 	short db;
 } calc_t;
 
-static void *calc_thread(void *arg) {
-	int channels = * (int*) arg;
-	int ret, pos = -1, i, c;
-	short *data;
-	char buf[128];
-	calc_t *dBs = malloc(channels * sizeof(calc_t));
-
-	fprintf(stderr, "%23s%9s%9s\n", "time", "channel1", "channel2");
-	
-	sem_wait(&sem);
-	while(is_running) {
-		pos ++;
-		if(pos >= SIZE) pos = 0;
-		
-		data = (short*) bufs[pos];
-		for(i = 0; i < channels; i++) dBs[i].sum = 0;
-		for(i = 0; i < g_frames * channels; i += channels) {
-			for(c = 0; c < channels; c ++) {
-				dBs[c].sum += abs(data[i + c]);
-			}
-		}
-		printf("%s", nowtime(buf, sizeof(buf)));
-		for(i = 0; i < channels; i++) {
-			dBs[i].db = dBs[i].sum * 500.0 / (g_frames * 32767.0);
-			if(dBs[i].db > 100) dBs[i].db = 100;
-			printf("%9d", dBs[i].db);
-		}
-		printf("\n");
-		
-		sem_wait(&sem);
-	}
-	
-	free(dBs);
-	
-	pthread_exit(NULL);
-}
+static GdkPixmap *pixmapVolume = NULL, *pixmapWave = NULL;
+static GtkObject *sigVolume = NULL, *sigWave = NULL;
 
 static void *cap_thread(void *arg) {
 	int ret, pos = -1;
@@ -204,11 +169,15 @@ static void *cap_thread(void *arg) {
 		} else if (ret < 0) {
 			fprintf(stderr, "error from writei: %s\n", snd_strerror(ret));
 			break;
+		} else if(ret != g_frames) {
+			fprintf(stderr, "write ret: %d\n", ret);
+			break;
 		} else {
-			sem_post(&sem);
-			if(ret != g_frames) {
-				fprintf(stderr, "write ret: %d\n", ret);
-				break;
+			if(!sigVolume || !pixmapVolume || !sigWave || !pixmapWave) {
+				pos = -1;
+			} else {
+				gtk_signal_emit_by_name(sigVolume, "da_event");
+				gtk_signal_emit_by_name(sigWave, "da_event");
 			}
 		}
 	}
@@ -216,87 +185,156 @@ static void *cap_thread(void *arg) {
 	pthread_exit(NULL);
 }
 
-static GtkWidget *window = NULL;
-static GdkPixmap *pixmap = NULL;
+static gboolean scribble_configure_event_volume(GtkWidget *widget, GdkEventConfigure *event, gpointer data) {
+	if(pixmapVolume) g_object_unref(G_OBJECT(pixmapVolume));
 
-static gboolean scribble_configure_event(GtkWidget *widget, GdkEventConfigure *event, gpointer data) {
-	if(pixmap) g_object_unref(G_OBJECT(pixmap));
-
-	pixmap = gdk_pixmap_new(widget->window, widget->allocation.width, widget->allocation.height, -1);
+	pixmapVolume = gdk_pixmap_new(widget->window, widget->allocation.width, widget->allocation.height, -1);
 	
-	gdk_draw_rectangle(pixmap, widget->style->white_gc, TRUE, 0, 0, widget->allocation.width, widget->allocation.height);
+	gdk_draw_rectangle(pixmapVolume, widget->style->white_gc, TRUE, 0, 0, widget->allocation.width, widget->allocation.height);
 	
 	return TRUE;
 }
 
-static gboolean scribble_expose_event(GtkWidget *widget, GdkEventExpose *event, gpointer data) {
-	gdk_draw_drawable(widget->window, widget->style->fg_gc[GTK_WIDGET_STATE(widget)], pixmap, event->area.x, event->area.y, event->area.x, event->area.y, event->area.width, event->area.height);
+static gboolean scribble_expose_event_volume(GtkWidget *widget, GdkEventExpose *event, gpointer data) {
+	gdk_draw_drawable(widget->window, widget->style->fg_gc[GTK_WIDGET_STATE(widget)], pixmapVolume, event->area.x, event->area.y, event->area.x, event->area.y, event->area.width, event->area.height);
 	
 	return FALSE;
 }
 
-static void draw_brush(GtkWidget *widget, gdouble x, gdouble y) {
+static void scribble_da_event_volume(GtkWidget *widget, GdkEventButton *event, gpointer _data) {
+	static int pos = -1;
+	int i, c;
+	short *data;
+	calc_t *dBs;
+	char buf[128];
+
 	GdkRectangle update_rect;
+
+	if(pixmapVolume == NULL) return;
+
+	pos ++;
+	if(pos >= SIZE) pos = 0;
+
+	update_rect.x = 0;
+	update_rect.y = 0;
+	update_rect.width = widget->allocation.width;
+	update_rect.height = widget->allocation.height;
+
+	printf("%s:%d %d\n", __func__, __LINE__, pos);
+
+	dBs = malloc(channels * sizeof(calc_t));
+
+	data = (short*) bufs[pos];
+	for(i = 0; i < channels; i++) dBs[i].sum = 0;
+	for(i = 0; i < g_frames * channels; i += channels) {
+		for(c = 0; c < channels; c ++) {
+			dBs[c].sum += abs(data[i + c]);
+		}
+	}
+
+	//printf("%s", nowtime(buf, sizeof(buf)));
+	for(i = 0; i < channels; i++) {
+		dBs[i].db = dBs[i].sum * 500.0 / (g_frames * 32767.0);
+		if(dBs[i].db > 100) dBs[i].db = 100;
+		// printf("%9d", dBs[i].db);
+	}
+	// printf("\n");
+
+	gdk_draw_rectangle(pixmapVolume, widget->style->white_gc, TRUE, 0, 0, widget->allocation.width, widget->allocation.height);
+
+	gint h = widget->allocation.height / channels;
+	for(i = 0; i < channels; i++) {
+		gdk_draw_rectangle(pixmapVolume, widget->style->black_gc, TRUE, 0, i * h, widget->allocation.width * dBs[i].db / 100, h);
+	}
+
+	gdk_window_invalidate_rect(widget->window, &update_rect, FALSE);
+
+	free(dBs);
+}
+
+static gboolean scribble_configure_event_wave(GtkWidget *widget, GdkEventConfigure *event, gpointer data) {
+	if(pixmapWave) g_object_unref(G_OBJECT(pixmapWave));
+
+	pixmapWave = gdk_pixmap_new(widget->window, widget->allocation.width, widget->allocation.height, -1);
 	
-	update_rect.x = x - 3;
-	update_rect.y = y - 3;
-	update_rect.width = 6;
-	update_rect.height = 6;
+	return TRUE;
+}
+
+static gboolean scribble_expose_event_wave(GtkWidget *widget, GdkEventExpose *event, gpointer data) {
+	gdk_draw_drawable(widget->window, widget->style->fg_gc[GTK_WIDGET_STATE(widget)], pixmapWave, event->area.x, event->area.y, event->area.x, event->area.y, event->area.width, event->area.height);
 	
-	gdk_draw_rectangle(pixmap, widget->style->black_gc, TRUE, update_rect.x, update_rect.y, update_rect.width, update_rect.height);
+	return FALSE;
+}
+
+static void scribble_da_event_wave(GtkWidget *widget, GdkEventButton *event, gpointer data) {
+	static int pos = -1;
+	GdkRectangle update_rect;
+
+	if(pixmapWave == NULL) return;
+
+	pos ++;
+	if(pos >= SIZE) pos = 0;
+
+	update_rect.x = 0;
+	update_rect.y = 0;
+	update_rect.width = widget->allocation.width;
+	update_rect.height = widget->allocation.height;
+
+	printf("%s:%d %d\n", __func__, __LINE__, pos);
+
+	gdk_draw_rectangle(pixmapWave, widget->style->white_gc, TRUE, 0, 0, widget->allocation.width, widget->allocation.height);
+
 	gdk_window_invalidate_rect(widget->window, &update_rect, FALSE);
 }
 
-static gboolean scribble_button_press_event(GtkWidget *widget, GdkEventButton *event, gpointer data) {
-	if(pixmap == NULL) return FALSE;
-	if(event->button == 1) draw_brush(widget, event->x, event->y);
-	return TRUE;
-}
-
-static gboolean scribble_motion_notify_event(GtkWidget *widget, GdkEventMotion *event, gpointer data) {
-	int x, y;
-	GdkModifierType state;
-	if(pixmap == NULL) return FALSE;
-	gdk_window_get_pointer(event->window, &x, &y, &state);
-	if(state & GDK_BUTTON1_MASK) draw_brush(widget, x, y);
-	return TRUE;
-}
-
 static void gtk_loop(int argc, char *argv[]) {
-	GtkWidget *frame;
+	GtkWidget *window;
 	GtkWidget *vbox;
-	GtkWidget *da;
-	GtkWidget *label;
+	GtkWidget *frameVolume, *daVolume;
+	GtkWidget *frameWave, *daWave;
 	
 	gtk_init(&argc, &argv);
+
+	g_signal_new("da_event", G_TYPE_OBJECT, G_SIGNAL_RUN_FIRST, 0, NULL, NULL, NULL, G_TYPE_NONE, 0);
 	
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(window), "Sound FFT");
 	g_signal_connect(G_OBJECT(window), "delete_event", G_CALLBACK(gtk_main_quit), NULL);
 	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 	
-	vbox = gtk_vbox_new(FALSE, 8);
-	gtk_container_set_border_width(GTK_CONTAINER(vbox), 8);
+	vbox = gtk_vbox_new(FALSE, 10);
+	gtk_container_set_border_width(GTK_CONTAINER(vbox), 0);
 	gtk_container_add(GTK_CONTAINER(window), vbox);
 	
-	label = gtk_label_new(NULL);
-	gtk_label_set_markup(GTK_LABEL(label), "<u>Area</u>");
-	gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 0);
+	frameVolume = gtk_frame_new(NULL);
+	gtk_frame_set_shadow_type(GTK_FRAME(frameVolume), GTK_SHADOW_IN);
+	gtk_box_pack_start(GTK_BOX(vbox), frameVolume, TRUE, TRUE, 0);
 	
-	frame = gtk_frame_new(NULL);
-	gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_IN);
-	gtk_box_pack_start(GTK_BOX(vbox), frame, TRUE, TRUE, 0);
+	daVolume = gtk_drawing_area_new();
+	gtk_widget_set_size_request(daVolume, 800, 50);
+	gtk_container_add(GTK_CONTAINER(frameVolume), daVolume);
 	
-	da = gtk_drawing_area_new();
-	gtk_widget_set_size_request(da, 800, 600);
-	gtk_container_add(GTK_CONTAINER(frame), da);
+	sigVolume = (GtkObject*) G_OBJECT(daVolume);
+	g_signal_connect(sigVolume, "expose_event", G_CALLBACK(scribble_expose_event_volume), NULL);
+	g_signal_connect(sigVolume, "configure_event", G_CALLBACK(scribble_configure_event_volume), NULL);
+	g_signal_connect(sigVolume, "da_event", G_CALLBACK(scribble_da_event_volume), NULL);
+
+	gtk_widget_set_events(daVolume, gtk_widget_get_events(daVolume) | GDK_LEAVE_NOTIFY_MASK);
+
+	frameWave = gtk_frame_new(NULL);
+	gtk_frame_set_shadow_type(GTK_FRAME(frameWave), GTK_SHADOW_IN);
+	gtk_box_pack_end(GTK_BOX(vbox), frameWave, TRUE, TRUE, 0);
 	
-	g_signal_connect(da, "expose_event", G_CALLBACK(scribble_expose_event), NULL);
-	g_signal_connect(da, "configure_event", G_CALLBACK(scribble_configure_event), NULL);
-	g_signal_connect(da, "motion_notify_event", G_CALLBACK(scribble_motion_notify_event), NULL);
-	g_signal_connect(da, "button_press_event", G_CALLBACK(scribble_button_press_event), NULL);
+	daWave = gtk_drawing_area_new();
+	gtk_widget_set_size_request(daWave, 800, 400);
+	gtk_container_add(GTK_CONTAINER(frameWave), daWave);
 	
-	gtk_widget_set_events(da, gtk_widget_get_events(da) | GDK_LEAVE_NOTIFY_MASK | GDK_BUTTON_PRESS_MASK | GDK_POINTER_MOTION_MASK | GDK_POINTER_MOTION_HINT_MASK);
+	sigWave = (GtkObject*) G_OBJECT(daWave);
+	g_signal_connect(sigWave, "expose_event", G_CALLBACK(scribble_expose_event_wave), NULL);
+	g_signal_connect(sigWave, "configure_event", G_CALLBACK(scribble_configure_event_wave), NULL);
+	g_signal_connect(sigWave, "da_event", G_CALLBACK(scribble_da_event_wave), NULL);
+	
+	gtk_widget_set_events(daWave, gtk_widget_get_events(daWave) | GDK_LEAVE_NOTIFY_MASK);
 	
 	gtk_widget_show_all(window);
 	gtk_main();
@@ -308,10 +346,10 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	int sample_rate = atoi(argv[1]);
-	int channels = atoi(argv[2]);
-	int format_size = atoi(argv[3]);
-	int ret = set_hardware_params(sample_rate, channels, format_size);
+	sample_rate = atoi(argv[1]);
+	channels = atoi(argv[2]);
+	format_size = atoi(argv[3]);
+	int ret = set_hardware_params();
 	if (ret < 0) {
 		fprintf(stderr, "set_hardware_params error\n");
 		return -1;
@@ -321,7 +359,6 @@ int main(int argc, char *argv[]) {
 
 	signal(SIGINT, sig_handle);
 	
-	sem_init(&sem, 0, 0);
 	memset(bufs, 0, sizeof(bufs));
 	for(int i = 0; i < SIZE; i ++) {
 		bufs[i] = (char*) malloc(g_frames * channels * format_size / 8);
@@ -339,12 +376,7 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "pthread_attr_setdetachstate failure: %d", ret);
 		return -1;
 	}
-	ret = pthread_create(&thread, &attr, calc_thread, &channels);
-	if(ret) {
-		fprintf(stderr, "pthread_create failure: %d", ret);
-		return -1;
-	}
-	ret = pthread_create(&thread2, &attr, cap_thread, NULL);
+	ret = pthread_create(&thread, &attr, cap_thread, NULL);
 	if(ret) {
 		fprintf(stderr, "pthread_create failure: %d", ret);
 		return -1;
@@ -353,15 +385,10 @@ int main(int argc, char *argv[]) {
 	gtk_loop(argc, argv);
 	
 	is_running = false;
-	sem_post(&sem);
 	if(pthread_join(thread, NULL)) {
 		perror("pthread_join failure");
 	}
-	if(pthread_join(thread2, NULL)) {
-		perror("pthread_join failure");
-	}
 	pthread_attr_destroy(&attr);
-	sem_destroy(&sem);
 
 	snd_pcm_drop(gp_handle);
 	snd_pcm_close(gp_handle);
