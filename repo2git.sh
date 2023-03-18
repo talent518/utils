41fa5203c4560856955235a635b563b0122bfafe
abao <talent518@yeah.net>
2023-03-19 01:59:50 +0800

    Update repo2git.sh

--- repo2git.sh
+++ repo2git.sh
@@ -6,11 +6,14 @@ Q=$(git config --global --default '' --get core.quotepath)
 
 git config --global core.quotepath false
 
+repo forall -c 'echo "$REPO_PATH" "$(echo $REPO_RREV|cut -d / -f3)"' > repo2git.lst
+CNT=$(cat repo2git.lst | wc -l)
+
 I=0
-I=$(repo forall -c 'echo "$REPO_PATH" "$(echo $REPO_RREV|cut -d / -f3)"' | while read dir ver; do
+$(cat repo2git.lst | while read dir ver; do
 	I=$(expr $I + 1)
 	echo
-	echo -e "\033[31m$I\e[0m \033[33m$dir\033[0m" >&2
+	echo -e "\033[34m$(expr $I \* 100 / $CNT)\033[0m% => \033[31m$I\e[0m/\033[32m$CNT\e[0m \033[33m$dir\033[0m" >&2
 
 	if [ -f "$dir.lock" ]; then
 		echo $(git -C "$dir" ls-files | wc -l) files >&2
@@ -18,12 +21,12 @@ I=$(repo forall -c 'echo "$REPO_PATH" "$(echo $REPO_RREV|cut -d / -f3)"' | while
 	fi
 
 	git -C "$dir" clean -fdx
-	git -C "$dir" ls-files > repo2git.lst
-	C=$(cat repo2git.lst|wc -l)
+	git -C "$dir" ls-files > repo2git.files
+	C=$(cat repo2git.files|wc -l)
 
 	if [ $C -gt 0 ]; then
 		N=0
-		cat repo2git.lst | while read line; do
+		cat repo2git.files | while read line; do
 			N=$(expr $N + 1)
 			echo -n -e "\033[2K$(expr $N \* 100 / $C)% => $N/$C\r" >&2
 			if [ "${line:0:1}" = '"' ]; then
@@ -42,7 +45,7 @@ I=$(repo forall -c 'echo "$REPO_PATH" "$(echo $REPO_RREV|cut -d / -f3)"' | while
 	fi
 done | wc -l)
 
-echo "completed $I projects"
+echo "completed $CNT projects"
 
 if [ ! -f ".lock" ]; then
 	I=0
@@ -51,11 +54,11 @@ if [ ! -f ".lock" ]; then
 		eval $dest
 		echo "\"$dest\""
 		echo -n -e "\033[2Kadd soft link or copy file for $I files\r">&2
-	done > repo2git.lst
-	if [ $(cat repo2git.lst|wc -l) -gt 0 ]; then
+	done > repo2git.files
+	if [ $(cat repo2git.files|wc -l) -gt 0 ]; then
 		echo
-		cat repo2git.lst | xargs git add -f
-		cat repo2git.lst | xargs git commit -v -m"add soft link or copy file" >&2
+		cat repo2git.files | xargs git add -f
+		cat repo2git.file | xargs git commit -v -m"add soft link or copy file" >&2
 		touch ".lock"
 	else
 		echo none soft link or copy file >&2
