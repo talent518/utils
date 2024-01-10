f49a575a22acbb783d053cb36fcd563adbf5b8df
abao <talent518@yeah.net>
2024-01-10 18:27:33 +0800

    Fix bug for pstore.c

--- pstore.c
+++ pstore.c
@@ -70,7 +70,7 @@ char *nowtime(void) {
 	localtime_r(&tv.tv_sec, &tm);
 
 	snprintf(buf, sizeof(buf), "%04d-%02d-%02d %02d:%02d:%02d.%06ld",
-		tm.tm_year + 1900, tm.tm_mon, tm.tm_mday,
+		tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
 		tm.tm_hour, tm.tm_min, tm.tm_sec,
 		tv.tv_usec
 	);
