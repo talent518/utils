01f1afb65cad8eb604271197a0a9b44c2b4501e3
阿宝 <parnter_zhangbaocai@saicmotor.com>
2024-03-15 12:46:33 +0800

    Fix bug for smart_str.c

--- smart_str.c
+++ smart_str.c
@@ -1,41 +1,65 @@
 #include "smart_str.h"
 
 bool _smart_str_put(smart_str_t *ss, const uint8_t *buf, uint16_t len) {
-	int i, j, n;
+	uint16_t i, remain, sz;
 
-	if(!len) return false;
+	if(!len)
+		return false;
 
-	n = (ss->tpos >= ss->rpos ? ss->tpos - ss->rpos : ss->size + ss->tpos - ss->rpos);
-
-	if(n + len >= ss->size) return false;
-
-	for(i = ss->tpos, j = 0; j < len; j++) {
-		ss->buf[i++] = buf[j];
-		if(i >= ss->size) i = 0;
+	if(ss->rpos > ss->tpos) {
+		remain = ss->rpos - ss->tpos;
+	} else {
+		remain = ss->size - ss->tpos + ss->rpos;
 	}
 
-	ss->tpos = i;
-	
+	if(remain <= len)
+		return false;
+
+	sz = ss->size - ss->tpos;
+	if(sz < len) {
+		memcpy(ss->buf + ss->tpos, buf, sz);
+		memcpy(ss->buf, buf + sz, len - sz);
+		ss->tpos = len - sz;
+	} else {
+		memcpy(ss->buf + ss->tpos, buf, len);
+
+		i = ss->tpos + len;
+		if(i == ss->size) i = 0;
+		ss->tpos = i;
+	}
+
 	return true;
 }
 
 uint16_t _smart_str_get(smart_str_t *ss, uint8_t *buf, uint16_t len) {
-	int i, j, n;
-	if(!len) return 0;
+	uint16_t i, written, sz;
+	if(!len)
+		return 0;
 
-	if(ss->rpos == ss->tpos) return 0;
+	if(ss->rpos == ss->tpos)
+		return 0;
 
-	n = (ss->tpos >= ss->rpos ? ss->tpos - ss->rpos : ss->size + ss->tpos - ss->rpos);
-
-	if(len > n) len = n;
-
-	for(i = ss->rpos, j = 0; j < len; j++) {
-		buf[j] = ss->buf[i++];
-		if(i >= ss->size) i = 0;
+	if(ss->tpos > ss->rpos) {
+		written = ss->tpos - ss->rpos;
+	} else {
+		written = ss->size - ss->rpos + ss->tpos;
 	}
 
-	ss->rpos = i;
+	if(len > written)
+		len = written;
+
+	sz = ss->size - ss->rpos;
+	if(sz < len) {
+		memcpy(buf, ss->buf + ss->rpos, sz);
+		memcpy(buf + sz, ss->buf, len - sz);
+		ss->rpos = len - sz;
+	} else {
+		memcpy(buf, ss->buf + ss->rpos, len);
+
+		i = ss->rpos + len;
+		if(i == ss->size) i = 0;
+		ss->rpos = i;
+	}
 
 	return len;
 }
-
