bc0e879882243747e469f7a54a29bbda6837436b
abao <talent518@yeah.net>
2024-11-02 17:57:29 +0800

    Update ffmpeg-setup.sh

--- ffmpeg-setup.sh
+++ ffmpeg-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# nvidia cuda options: --enable-cuda-nvcc --enable-cuvid --enable-ffnvcodec --enable-libnpp --enable-nvdec --enable-nvenc --enable-vdpau --enable-nonfree
+#                      --nvccflags="-gencode arch=compute_86,code=sm_86 -O2"
+
 set -e
 
 sudo apt install -y build-essential libgnutls28-dev libmp3lame-dev libx264-dev libopus-dev libvpx-dev libssl-dev \
@@ -20,6 +23,7 @@ sudo apt install -y build-essential libgnutls28-dev libmp3lame-dev libx264-dev l
 	libzmq3-dev libzvbi-dev liblv2dynparam1-dev libomxil-bellagio-dev libopenal-dev libopencl-clang-dev libopengl-dev libsdl2-dev libpocketsphinx-dev \
 	opencl-dev \
 	libjack-dev libcdio-paranoia-dev libcdio-dev \
+	libdrm-dev \
 	yasm nasm
 
 ./configure --prefix=/opt/ffmpeg --enable-gpl --disable-stripping --enable-gnutls --enable-ladspa --enable-libaom --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcdio --enable-libcodec2 --enable-libdav1d --enable-libflite --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libgme --enable-libgsm --enable-libjack --enable-libmp3lame --enable-libmysofa --enable-libopenjpeg --enable-libopenmpt --enable-libopus --enable-libpulse --enable-librabbitmq --enable-librubberband --enable-libshine --enable-libsnappy --enable-libsoxr --enable-libspeex --enable-libsrt --enable-libssh --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvorbis --enable-libvpx --enable-libwebp --enable-libx265 --enable-libxml2 --enable-libxvid --enable-libzimg --enable-libzmq --enable-libzvbi --enable-lv2 --enable-omx --enable-openal --enable-opencl --enable-opengl --enable-sdl2 --enable-pocketsphinx --enable-librsvg --enable-libdc1394 --enable-libdrm --enable-chromaprint --enable-frei0r --enable-libx264 --enable-libdrm --disable-rkmpp --enable-version3 --disable-libopenh264 --disable-vaapi --disable-vdpau --disable-decoder=h264_v4l2m2m --disable-decoder=vp8_v4l2m2m --disable-decoder=mpeg2_v4l2m2m --disable-decoder=mpeg4_v4l2m2m --enable-doc --enable-libv4l2 $@
