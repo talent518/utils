f960a98f87efe9c05b7cdd11ac0c78cf77287f54
阿宝 <parnter_zhangbaocai@saicmotor.com>
2024-11-21 02:43:43 +0800

    Optimize for pstack.sh

--- pstack.sh
+++ pstack.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 pgrep $@ | while read p; do
+  echo '##############################'
+  echo -n '### '
+  ps -o cmd -p $p --no-heading
+  echo '##############################'
   gdb -q -p $p -ex 'thread apply all bt' -ex 'set confirm off' -ex q
 done
